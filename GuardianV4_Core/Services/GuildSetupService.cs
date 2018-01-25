ef65c9f623ffbe450fe46b209df498d923a82d7b
jaetpa <jaetpa@gmail.com>
2018-01-25 15:04:51 +0000

    Automatically adds new guilds to database

---
+++ GuardianV4_Core/Services/GuildSetupService.cs
@@ -0,0 +1,42 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuardianV4_Core.Services
+{
+    public class GuildSetupService
+    {
+        DiscordSocketClient _client;
+        DatabaseService _db;
+        public GuildSetupService(DiscordSocketClient client, DatabaseService db)
+        {
+            _client = client;
+            _db = db;
+
+            _client.GuildAvailable += EnsureGuildInDb;
+        }
+
+        private Task EnsureGuildInDb(SocketGuild arg)
+        {
+            using (var uow = _db.UnitOfWork)
+            {
+                var entity = uow.Servers.Find(arg.Id);
+
+                if (entity == null)
+                {
+                    uow.Servers.Add(new GuardianV4_Data.Entities.ServerEntity { Id = arg.Id, GuildName = arg.Name });
+                    uow.SaveChanges();
+                }
+                else if (entity.GuildName != arg.Name)
+                {
+                    uow.Servers.Update(entity);
+                    entity.GuildName = arg.Name;
+                    uow.SaveChanges();
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
