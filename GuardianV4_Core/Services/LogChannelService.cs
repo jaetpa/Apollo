33dab0397901e80e9147489013919ad7fbe7f68b
jaetpa <jaetpa@gmail.com>
2018-01-25 18:28:17 +0000

    Implement logging for username and nickname changes

--- GuardianV4_Core/Services/LogChannelService.cs
+++ GuardianV4_Core/Services/LogChannelService.cs
@@ -16,7 +16,12 @@ namespace GuardianV4_Core.Services
             _client = client;
 
             _client.GuildAvailable += LogConnected;
+            _client.UserUpdated += LogUsernameChanges;
+            _client.GuildMemberUpdated += LogNicknameChanges;
+
         }
+
+
 
         private async Task LogConnected(SocketGuild arg)
         {
@@ -29,6 +34,56 @@ namespace GuardianV4_Core.Services
                 .WithEmbedType(EmbedType.Connect, _client.CurrentUser)
                 .Build();
             await logChannel.SendMessageAsync("", embed: embed);
+        }
+
+        private async Task LogUsernameChanges(SocketUser arg1, SocketUser arg2)
+        {
+            if (arg1.Username == arg2.Username)
+            {
+                return;
+            }
+            else
+            {
+                var embed = new EmbedBuilder()
+                    .WithEmbedType(EmbedType.UsernameChange, arg2)
+                    .WithDescription($"User **{arg1}** changed username to **{arg2}**")
+                    .WithTimestamp()
+                    .Build();
+                await (arg2 as SocketGuildUser).Guild.GetLogChannel().SendMessageAsync("", embed: embed);
+            }
+        }
+
+        private async Task LogNicknameChanges(SocketUser arg1, SocketUser arg2)
+        {
+            var user1 = arg1 as SocketGuildUser;
+            var user2 = arg2 as SocketGuildUser;
+
+            if (user1.Nickname == user2.Nickname)
+            {
+                return;
+            }
+            else
+            {
+                string message = String.Empty;
+                if (user1.Nickname == null)
+                {
+                    message = $"User **{arg2}** set nickname **{user2.Nickname}#{arg2.Discriminator}**";
+                }
+                else if (user2.Nickname == null)
+                {
+                    message = $"User **{arg2}** removed nickname **{user1.Nickname}#{arg1.Discriminator}**";
+                }
+                else
+                {
+                    message = $"User **{arg2}** changed nickname from **{user1.Nickname}#{arg1.Discriminator}** to **{user2.Nickname}#{arg2.Discriminator}**";
+                }
+                var embed = new EmbedBuilder()
+                    .WithEmbedType(EmbedType.NicknameChange, arg2)
+                    .WithDescription(message)
+                    .WithTimestamp()
+                    .Build();
+                await (arg2 as SocketGuildUser).Guild.GetLogChannel().SendMessageAsync("", embed: embed);
+            }
         }
     }
 }
