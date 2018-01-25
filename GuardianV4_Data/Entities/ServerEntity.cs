c438782919050cf94c89f10afacee6ab93f9283d
jaetpa <jaetpa@gmail.com>
2018-01-25 20:52:42 +0000

    Add WelcomeChannelId to Servers table

--- GuardianV4_Data/Entities/ServerEntity.cs
+++ GuardianV4_Data/Entities/ServerEntity.cs
@@ -8,12 +8,12 @@ namespace GuardianV4_Data.Entities
     {
         public string GuildName { get; set; }
         public ulong? MainChannelId { get; set; }
+        public ulong? WelcomeChannelId { get; set; }
         public ulong? StaffChannelId { get; set; }
         public ulong? BotChannelId { get; set; }
         public ulong? LogChannelId { get; set; }
         public ulong? DeleteLogChannelId { get; set; }
         public ulong? VoiceTextChannelId { get; set; }
-
 
         public virtual ICollection<QuoteEntity> Quotes { get; set; }
     }
