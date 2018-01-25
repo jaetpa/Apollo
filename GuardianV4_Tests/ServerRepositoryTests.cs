6d3d913f2c5a2b369a74309daab927079dc167d9
jaetpa <jaetpa@gmail.com>
2018-01-25 23:11:00 +0000

    Update test...test method

--- GuardianV4_Tests/ServerRepositoryTests.cs
+++ GuardianV4_Tests/ServerRepositoryTests.cs
@@ -2,34 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-//using GuardianV4_Data.Entities;
-//using GuardianV4_Repository;
-//using GuardianV4_Repository.Unit_of_Work;
+using GuardianV4_Data.Entities;
+using GuardianV4_Repository;
+using GuardianV4_Repository.Unit_of_Work;
 using NUnit.Framework;
+using Microsoft.Extensions.DependencyInjection;
+using GuardianV4_Data.Contexts;
 
 namespace GuardianV4_Tests
 {
+
     [TestFixture]
-    class ServerRepositoryTests
+    public class ServerRepositoryTests
     {
-        //    [Test]
-        //    void Add_ValidServerEntity_AddedToDatabase(UnitOfWork uow)
-        //    {
-        //        //Arrange
-        //        var server = new ServerEntity { Id = 3434145463624262 };
-
-        //        //Act
-        //        uow.Servers.Add(server);
-        //        uow.SaveChanges();
-
-        //        //Assert
-        //        Assert.Contains(server, uow.Context.Servers.ToList());
-        //    }
+        IServiceProvider _services;
 
         [Test]
-        void Test()
+        public void Add_ValidServerEntity_AddedToDatabase()
         {
-            Assert.True(true);
+            //Arrange
+            var server = new ServerEntity { Id = 3434145463624262 };
+            _services.GetRequiredService<DiscordBotContext>();
+            var uow = _services.GetRequiredService<UnitOfWork>();
+            //Act
+            uow.Servers.Add(server);
+            uow.SaveChanges();
+
+            //Assert
+            Assert.Contains(server, uow.Context.Servers.ToList());
         }
     }
 
