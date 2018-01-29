09645684d20a7741e1b43044f9af23f97cf360e8
jaetpa <jaetpa@gmail.com>
2018-01-29 21:44:38 +0000

    Implement mute commands:

--- GuardianV4_Core/Modules/Moderation/ModeratorModule.UserManagement.cs
+++ GuardianV4_Core/Modules/Moderation/ModeratorModule.UserManagement.cs
@@ -4,6 +4,7 @@ using Discord.WebSocket;
 using GuardianV4_Core.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -48,6 +49,57 @@ namespace GuardianV4_Core.Modules.Moderation
             await Context.Guild.GetLogChannel().SendMessageAsync("", embed: embed);
 
         }
+
+        [Command("mute")]
+        [Summary("Mutes a user. They will not be able to speak in text or voice channels.")]
+        [Remarks("!mute @florin_ro#9196")]
+        public async Task MuteUser(SocketGuildUser user, [Remainder] string reason = null)
+        {
+            var mutedRole = await Context.Guild.GetOrCreateMutedRole();
+
+            if (user.Roles.FirstOrDefault(role => role.Id == mutedRole.Id) != null)
+            {
+                await ReplyAsync($"**{user}** is already muted.");
+                return;
+            }
+
+            await user.AddRoleAsync(mutedRole);
+
+            var embed = new EmbedBuilder()
+                .WithEmbedType(EmbedType.Mute, user)
+                .WithDescription($"User **{user}** was muted.\n" +
+                    $"Issuer: **{Context.User}**\n" +
+                    $"Reason: {reason ?? "none specified"}")
+                .Build();
+
+            await ReplyAsync("", embed: embed);
+            await Context.Guild.GetLogChannel().SendMessageAsync("", embed: embed);
+        }
+
+        [Command("unmute")]
+        [Summary("Unmutes a user. They will be able to speak again.")]
+        [Remarks("!unmute @florin_ro#9196")]
+        public async Task UnmuteUser(SocketGuildUser user)
+        {
+            var mutedRole = await Context.Guild.GetOrCreateMutedRole();
+            if (user.Roles.FirstOrDefault(role => role.Id == mutedRole.Id) == null)
+            {
+                await ReplyAsync($"**{user}** is not muted.");
+                return;
+            }
+
+            await user.RemoveRoleAsync(mutedRole);
+
+            var embed = new EmbedBuilder()
+                .WithEmbedType(EmbedType.Mute, user)
+                .WithDescription($"User **{user}** was muted.\n" +
+                    $"Issuer: **{Context.User}**\n")
+                .Build();
+
+            await ReplyAsync("", embed: embed);
+            await Context.Guild.GetLogChannel().SendMessageAsync("", embed: embed);
+        }
+
 
     }
 }
