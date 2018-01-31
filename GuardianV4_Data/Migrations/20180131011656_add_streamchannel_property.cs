3d73d7952813c40b5df0114cdae85ef03cf13d68
jaetpa <jaetpa@gmail.com>
2018-01-31 18:10:24 +0000

    Add StreamChannel column to servers table

---
+++ GuardianV4_Data/Migrations/20180131011656_add_streamchannel_property.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GuardianV4_Data.Migrations
+{
+    public partial class add_streamchannel_property : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "StreamTextChannelId",
+                table: "Servers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StreamTextChannelId",
+                table: "Servers");
+        }
+    }
+}
