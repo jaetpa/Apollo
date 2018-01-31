ef1d040044b1d46053123508ce324b55c47436cb
jaetpa <jaetpa@gmail.com>
2018-01-31 20:21:44 +0000

    Add custom commands to unit of work

---
+++ GuardianV4_Core/Modules/Moderation/ModeratorModule.CustomCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using GuardianV4_Core.Services;
+using Newtonsoft.Json;
+
+namespace GuardianV4_Core.Modules.Moderation
+{
+    public partial class ModeratorModule
+    {
+        [Command("addcommand")]
+        public async Task AddCommand(string commandName)
+        {
+            using (var uow = _db.UnitOfWork)
+            {
+                if (uow.CustomCommands.Find())
+                {
+
+                }
+            }
+        }
+    }
+}
