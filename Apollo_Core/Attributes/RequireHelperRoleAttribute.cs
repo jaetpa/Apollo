95e55a1b4821dd9beaf10c45e96f81f31cbf2efc
jaetpa <jaetpa@gmail.com>
2018-02-09 00:50:40 +0000

    Add precondition for Helper role

---
+++ Apollo_Core/Attributes/RequireHelperRoleAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollo_Core.Attributes
+{
+    class RequireHelperRoleAttribute : PreconditionAttribute
+    {
+        public override async Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            if (context.User is SocketGuildUser user)
+            {
+                if (user.Roles.Any(x => x.Name.ToUpper() == "HELPERS")
+                    || user.Roles.Any(x => x.Name.ToUpper() == "CONDUCTORS"))
+                {
+                    return PreconditionResult.FromSuccess();
+                }
+                else
+                {
+                    return PreconditionResult.FromError("You do not have the required role.");
+                }
+            }
+            else
+            {
+                return PreconditionResult.FromError("This command must be executed in a guild.");
+            }
+        }
+    }
+}
