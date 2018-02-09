76b623695f5ef1e9fc875578449f1236b365f5f6
jaetpa <jaetpa@gmail.com>
2018-02-09 01:00:36 +0000

    Move to DiscordBot namespace

--- Apollo_Core/Attributes/RequireHelperRoleAttribute.cs
+++ Apollo_Core/Attributes/RequireHelperRoleAttribute.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Apollo_Core.Attributes
+namespace DiscordBot_Core.Attributes
 {
     class RequireHelperRoleAttribute : PreconditionAttribute
     {
