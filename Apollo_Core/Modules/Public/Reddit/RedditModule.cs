030a7b80b87125d2340c99846b6bf16df3b27423
jaetpa <jaetpa@gmail.com>
2018-02-19 19:52:01 +0000

    Move AskReddit methods to RedditModule.AskReddit

--- Apollo_Core/Modules/Public/Reddit/RedditModule.cs
+++ Apollo_Core/Modules/Public/Reddit/RedditModule.cs
@@ -11,7 +11,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DiscordBot_Core.Modules.Reddit.Public
+namespace DiscordBot_Core.Modules.Public.Reddit
 {
     public partial class RedditModule : ModuleBase<SocketCommandContext>
     {
@@ -24,86 +24,6 @@ namespace DiscordBot_Core.Modules.Reddit.Public
             _redditService = redditService;
             _reddit = _redditService.RedditApi;
             _db = db;
-        }
-
-        [RequireModeratorRole]
-        [Command("ap", RunMode = RunMode.Async)]
-        [Summary("Gets a random post from r/askreddit and posts to default channel")]
-        [Remarks("!ap")]
-        public async Task AskRedditInDefaultChannel([Remainder] string topic = null)
-        {
-            var mainChannel = Context.Guild.GetMainChannel();
-
-            if (mainChannel == null)
-            {
-                await ReplyAsync("Main channel has not been set.");
-                return;
-            }
-            try
-            {
-                var links = await SearchReddit(subreddit: "askreddit", topic: topic);
-                var rand = new Random();
-                await mainChannel.SendMessageAsync((links.ElementAt(rand.Next(links.Count())) as Link).Title);
-            }
-            catch (HttpRequestException)
-            {
-                await ReplyAsync("There was a problem reaching Reddit servers. Please try again later.");
-            }
-        }
-
-
-        [Command("askreddit", RunMode = RunMode.Async)]
-        [Summary("Gets a random post from r/askreddit")]
-        [Remarks("!askreddit")]
-        public async Task AskReddit([Remainder] string topic = null)
-        {
-            var rand = new Random();
-            try
-            {
-                var links = await SearchReddit(subreddit: "askreddit", topic: topic);
-                await ReplyAsync((links.ElementAt(rand.Next(links.Count())) as Link).Title);
-            }
-            catch (HttpRequestException)
-            {
-                await ReplyAsync("There was a problem reaching Reddit servers. Please try again later.");
-                return;
-            }
-        }
-
-        [RequireModeratorRole]
-        [Command("askreddit", RunMode = RunMode.Async)]
-        [Summary("Gets a random post from r/askreddit")]
-        [Remarks("!askreddit")]
-        public async Task AskReddit(int count = 1, [Remainder] string topic = null)
-        {
-            try
-            {
-                var links = await SearchReddit(subreddit: "askreddit", topic: topic);
-                List<Link> randomLinks = new List<Link>();
-                var rand = new Random();
-                int attempts = 0;
-
-                do
-                {
-                    randomLinks.Add(links.ElementAtOrDefault(rand.Next(links.Count())) as Link);
-                    attempts++;
-                } while (randomLinks.Count < count && (attempts < 50));
-
-
-                StringBuilder stringBuilder = new StringBuilder();
-
-                foreach (var link in randomLinks)
-                {
-                    stringBuilder.Append((link as Link).Title + "\n");
-                }
-
-                await ReplyAsync(stringBuilder.ToString());
-            }
-            catch (HttpRequestException)
-            {
-                await ReplyAsync("There was a problem reaching Reddit servers. Please try again later.");
-                return;
-            }
         }
 
         private async Task<IEnumerable<Thing>> SearchReddit(string subreddit = null, int count = 25, string topic = null)
@@ -122,5 +42,6 @@ namespace DiscordBot_Core.Modules.Reddit.Public
 
             return results;
         }
+
     }
 }
